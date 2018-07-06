bba2d728b39c7d9c9de044f619f7713cda913c93
rambogoff <rmznurkmez91@gmail.com>
2018-07-06 14:02:58 +0300

    Dictionary_Pandas_LogicControl.py

--- Dictionary_Pandas_LogicControl.py
+++ Dictionary_Pandas_LogicControl.py
@@ -6,29 +6,29 @@ import matplotlib.pyplot as plt
 import seaborn as sns  # visualization tool
 
 
-#dictionary = {"Turkey" : "Ankara", "Spain" : "Madrid"}
-#print(dictionary.keys())
-#print(dictionary.values())
+dictionary = {"Turkey" : "Ankara", "Spain" : "Madrid"}
+print(dictionary.keys())
+print(dictionary.values())
 #
-#dictionary['Turkey'] = "Kirikkale" # Update
-#dictionary['France'] = 'Paris' #Add new entry
-#del dictionary ['Spain'] #remove entry
-#print ('France' in dictionary) # Search results:(True/False)
-#dictionary.clear() # remove all
-#print(dictionary)
+dictionary['Turkey'] = "Kirikkale" # Update
+dictionary['France'] = 'Paris' #Add new entry
+del dictionary ['Spain'] #remove entry
+print ('France' in dictionary) # Search results:(True/False)
+dictionary.clear() # remove all
+print(dictionary)
 
 
 
 #%% Pandas CSV (Comma- separated values)
 data = pd.read_csv("pokemon.csv") # pandas kütüphanesini kullanarak pokemon.csv import ettik
 
-#seriler vektör şeklinde tek boyutlu
+# seriler vektör şeklinde tek boyutlu
 #dataFrameler iki boyutlu
 #
-#series = data['Defense']  #series [""] = Series
-#print(type(series))
-#data_frame = data[['Defense']]  #data[[""]] = dataFrame
-#print(type(data_frame))
+series = data['Defense']  #series [""] = Series
+print(type(series))
+data_frame = data[['Defense']]  #data[[""]] = dataFrame
+print(type(data_frame))
 
 
 # ==, <, >, <=, >=
@@ -36,15 +36,15 @@ data = pd.read_csv("pokemon.csv") # pandas kütüphanesini kullanarak pokemon.cs
 
 #%% Filtering
 
-#x = data['Defense'] > 200
-#data[x]
+x = data['Defense'] > 200
+data[x]
 
 
 #1.Path
-#data[np.logical_and(data['Defense'] >200,
-#                   data['Attack'] > 100)]
+data[np.logical_and(data['Defense'] >200,
+                   data['Attack'] > 100)]
 #2.path
-#data[(data['Defense'] > 200) &(data['Attack'] > 100) ]
+data[(data['Defense'] > 200) &(data['Attack'] > 100) ]
 
 
 
